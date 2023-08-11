c2239132562f258deb430a51cf46e3ceca6ae6f7
andrei c <andr@home.com>
2023-08-11 16:41:05 +0300

    added methods to get by host and by host & date

--- app/Http/Controllers/MeetingAppointmentController.php
+++ app/Http/Controllers/MeetingAppointmentController.php
@@ -93,15 +93,18 @@ class MeetingAppointmentController extends Controller
         }
     }
 
-    public function getAppointmentsByHost(string $hostUserId)
+    // Returns all active appointmnets hosted (created) bt a certain user
+    public function getActiveAppointmentsByHost(string $hostUserId)
     {
 
         $appointments = MeetingAppointment::whereHas('meetingEvent',
-            function (Builder $query) use($hostUserId)
-            {
-                $query->where('host_user_id_fk', $hostUserId);
-            }
-        )->get();
+                                function (Builder $query) use($hostUserId)
+                                {
+                                    $query->where('host_user_id_fk', $hostUserId);
+                                }
+                            )
+                            ->where('active', 1) // active == true
+                            ->get();
 
         foreach ($appointments as $appointment)
         {
@@ -111,14 +114,14 @@ class MeetingAppointmentController extends Controller
         return response()->json($appointments, 200);
     }
 
-    /* this method sends all meeting who's host ID is the $hostUserId and which belong to the 
-    year and month specified in the JSON request body
-    JSON request body should look like this:
+    /* Returns all active appointmnets hosted (created) bt a certain user in a certain
+    year and month specified in the JSON request body.
+    The JSON request body should look like this (example):
     {
         "year": 2023,
         "month": 10
     } */ 
-    public function getAppointmentsByHostAndDate(Request $request, string $hostUserId)
+    public function getActiveAppointmentsByHostAndDate(Request $request, string $hostUserId)
     {
         $appointments = MeetingAppointment::whereHas('meetingEvent',
                                 function (Builder $query) use($hostUserId)
@@ -126,6 +129,7 @@ class MeetingAppointmentController extends Controller
                                     $query->where('host_user_id_fk', $hostUserId);
                                 }
                             )
+                            ->where('active', 1) // active == true
                             ->whereYear('start', $request->input('year'))
                             ->whereMonth('start', $request->input('month'))
                             ->get();
