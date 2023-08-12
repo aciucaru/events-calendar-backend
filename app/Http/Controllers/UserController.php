05eb1ecb65c17117d264093139bb7d142bc3dc0a
andrei c <andr@home.com>
2023-08-12 13:03:21 +0300

    finished getHostedMeetingsBydDate method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Invitation;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use App\Models\User;
 
 use App\Models\MeetingAppointment;
+use App\Models\MeetingEvent;
 
 class UserController extends Controller
 {
@@ -97,47 +99,119 @@ class UserController extends Controller
         }
     }
 
-        // Returns all active appointmnets hosted (created) bt a certain user
-        public function getActiveHostedAppointments(string $hostUserId)
-        {
+    public function getHostedMeetings(string $hostUserId)
+    {
+        $meetingEvents = MeetingEvent::where('host_user_id_fk', $hostUserId)->get();
+
+        return response()->json($meetingEvents, 200);
+    }
+
     
-            $appointments = MeetingAppointment::whereHas('meetingEvent',
-                                    function (Builder $query) use($hostUserId)
+    public function getHostedMeetingsBydDate(Request $request, string $hostUserId)
+    {
+        $validatedRequestData = $request->validate(
+            [
+                'year' => [ 'required', 'integer', 'numeric', 'min:1900' ],
+                'month' => [ 'required', 'integer', 'numeric', 'min:1' , 'max:12'], // month is between 1...12
+            ]
+        );
+
+        $hostedMeetings = 
+            MeetingEvent::where('host_user_id_fk', $hostUserId)
+                        ->whereHas('meetingAppointments',
+                                    function (Builder $query) use ($validatedRequestData)
                                     {
-                                        $query->where('host_user_id_fk', $hostUserId);
+                                        $query->whereYear('start', $validatedRequestData['year'])
+                                                ->whereMonth('start', $validatedRequestData['month']);
                                     }
                                 )
-                                ->where('active', 1) // active == true
-                                ->get();
-    
-            foreach ($appointments as $appointment)
+                        ->get();
+
+        return response()->json($hostedMeetings, 200);
+    }
+
+    // Returns all active appointmnets hosted (created) bt a certain user
+    public function getActiveHostedAppointments(string $hostUserId)
+    {
+
+        $appointments = MeetingAppointment::whereHas('meetingEvent',
+                                function (Builder $query) use($hostUserId)
+                                {
+                                    $query->where('host_user_id_fk', $hostUserId);
+                                }
+                            )
+                            ->where('active', 1) // active == true
+                            ->get();
+
+        foreach ($appointments as $appointment)
+        {
+            $appointment->meetingEvent;
+        };
+
+        return response()->json($appointments, 200);
+    }
+
+    /* Returns all active appointmnets hosted (created) bt a certain user in a certain
+    year and month specified in the JSON request body.
+    The JSON request body should look like this (example):
+    {
+        "year": 2023,
+        "month": 10
+    } */ 
+    public function getActiveHostedAppointmentsByDate(Request $request, string $hostUserId)
+    {
+        $appointments = MeetingAppointment::whereHas('meetingEvent',
+                                function (Builder $query) use($hostUserId)
+                                {
+                                    $query->where('host_user_id_fk', $hostUserId);
+                                }
+                            )
+                            ->where('active', 1) // active == true
+                            ->whereYear('start', $request->input('year'))
+                            ->whereMonth('start', $request->input('month'))
+                            ->get();
+
+        return response()->json($appointments, 200);
+    }
+
+        /* Returns all invitations to which a user was invited to and which start at a certain 
+    year and month specified in the JSON request body.
+    The JSON request body should look like this (example):
+    {
+        "year": 2023,
+        "month": 10
+    } */
+    public function getActiveInvitationsByDate(Request $request, string $guestUserId)
+    {
+        $validatedRequestData = $request->validate(
+            [
+                'year' => [ 'required', 'integer', 'numeric', 'min:1900' ],
+                'month' => [ 'required', 'integer', 'numeric', 'min:1' , 'max:12'], // month is between 1...12
+            ]
+        );
+
+        $invitations = Invitation::whereHas('meetingAppointment',
+                function (Builder $query) use($request)
+                {
+                    $query->whereYear('start', $request->input('year'))
+                            ->whereMonth('start', $request->input('month'))
+                            ->where('active', 1); // only invitation belongging to active appointment
+                }
+            )
+            ->where('guest_user_id_fk', $guestUserId)
+            ->get();
+
+        $invitations = Invitation::whereHas('meetingAppointment',
+            function (Builder $query) use($validatedRequestData)
             {
-                $appointment->meetingEvent;
-            };
-    
-            return response()->json($appointments, 200);
-        }
-    
-        /* Returns all active appointmnets hosted (created) bt a certain user in a certain
-        year and month specified in the JSON request body.
-        The JSON request body should look like this (example):
-        {
-            "year": 2023,
-            "month": 10
-        } */ 
-        public function getActiveHostedAppointmentsByDate(Request $request, string $hostUserId)
-        {
-            $appointments = MeetingAppointment::whereHas('meetingEvent',
-                                    function (Builder $query) use($hostUserId)
-                                    {
-                                        $query->where('host_user_id_fk', $hostUserId);
-                                    }
-                                )
-                                ->where('active', 1) // active == true
-                                ->whereYear('start', $request->input('year'))
-                                ->whereMonth('start', $request->input('month'))
-                                ->get();
-    
-            return response()->json($appointments, 200);
-        }
+                $query->whereYear('start', $validatedRequestData['year'])
+                        ->whereMonth('start', $validatedRequestData['month'])
+                        ->where('active', 1); // only invitation belongging to active appointment
+            }
+        )
+        ->where('guest_user_id_fk', $guestUserId)
+        ->get();
+
+        return response()->json($invitations, 200); // 200 - succesfull request
+    }
 }
