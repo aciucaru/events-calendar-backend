82e7103390ff0edb1a0c64214c8938ebef4d473f
andrei c <andr@home.com>
2023-08-13 15:17:30 +0300

    corrected whereBelongsTo() with whereHas()

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -193,30 +193,7 @@ class UserController extends Controller
         );
 
         $invitations = Invitation::whereHas('meetingAppointment',
-                function (Builder $query) use($request)
-                {
-                    $query->whereYear('start', $request->input('year'))
-                            ->whereMonth('start', $request->input('month'))
-                            ->where('active', 1); // only invitation belongging to active appointment
-                }
-            )
-            ->where('guest_user_id_fk', $guestUserId)
-            ->get();
-
-        // $invitations = Invitation::whereHas('meetingAppointment',
-        //     function (Builder $query) use($validatedRequestData)
-        //     {
-        //         $query->whereYear('start', $validatedRequestData['year'])
-        //                 ->whereMonth('start', $validatedRequestData['month'])
-        //                 ->where('active', 1); // only invitation belongging to active appointment
-        //     }
-        // )
-        // ->where('guest_user_id_fk', $guestUserId)
-        // ->get();
-
-        // this should be the correct version
-        $invitations = Invitation::whereBelongsTo('meetingAppointment',
-        function (Builder $query) use($validatedRequestData)
+            function (Builder $query) use($validatedRequestData)
             {
                 $query->whereYear('start', $validatedRequestData['year'])
                         ->whereMonth('start', $validatedRequestData['month'])
@@ -225,6 +202,18 @@ class UserController extends Controller
         )
         ->where('guest_user_id_fk', $guestUserId)
         ->get();
+
+        // this should be the correct version but it DOES NOT WORK!
+        // $invitations = Invitation::whereBelongsTo('meetingAppointment',
+        // function (Builder $query) use($validatedRequestData)
+        //     {
+        //         $query->whereYear('start', $validatedRequestData['year'])
+        //                 ->whereMonth('start', $validatedRequestData['month'])
+        //                 ->where('active', 1); // only invitation belongging to active appointment
+        //     }
+        // )
+        // ->where('guest_user_id_fk', $guestUserId)
+        // ->get();
 
         return response()->json($invitations, 200); // 200 - succesfull request
     }
