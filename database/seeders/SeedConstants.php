99490d00af6296afa6ec38458109d4cdd67cc6aa
andrei c <andr@home.com>
2023-08-15 19:25:33 +0300

    adjusted seed setting

--- database/seeders/SeedConstants.php
+++ database/seeders/SeedConstants.php
@@ -10,8 +10,8 @@ class SeedConstants
     public const WEEKS_WITH_EVENTS = 8;
     public const DAYS_PER_WEEK_WITH_EVENTS = 5;
 
-    public const USERS_PER_DAY_WITH_OUT_OF_OFFICE_EVENTS = 2;
-    public const HOST_USERS_PER_DAY_WITH_MEETINGS = 8;
+    public const USERS_PER_DAY_WITH_OUT_OF_OFFICE_EVENTS = 4;
+    public const HOST_USERS_PER_DAY_WITH_MEETINGS = 10;
     public const TOTAL_USERS_PER_DAY_WITH_EVENTS = self::USERS_PER_DAY_WITH_OUT_OF_OFFICE_EVENTS
                                                     + self::HOST_USERS_PER_DAY_WITH_MEETINGS;
 
@@ -25,7 +25,7 @@ class SeedConstants
     public const APPOINTMENTS_COUNT = self::MEETINGS_COUNT;
     
     public const MIN_INVITATIONS_PER_APPOINTMENT = 4;
-    public const MAX_INVITATIONS_PER_APPOINTMENT = 10;
+    public const MAX_INVITATIONS_PER_APPOINTMENT = 8;
     
     public const PROJECT_COUNT = 20;
 
